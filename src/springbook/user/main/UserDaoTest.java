0f924267511a45431bf1c0c3af3933b45bb801e1
Rupinel <rupinel@gmail.com>
2017-07-20 18:02:48 +0900

    170720 DAO 내add(), get() unit 테스트

--- src/springbook/user/main/UserDaoTest.java
+++ src/springbook/user/main/UserDaoTest.java
@@ -19,11 +19,10 @@ public class UserDaoTest {
 	
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao",UserDao.class);
-		UserDao dao2 = context.getBean("userDao",UserDao.class);
 		
 		User user = new User();
 		user.setId("whiteship");
-		user.setName("백기선");
+		user.setName("name");
 		user.setPassword("married");
 		
 		dao.add(user);
@@ -31,11 +30,14 @@ public class UserDaoTest {
 		System.out.println(user.getId() + " 등록 성공");
 		
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
 		
-		System.out.println(user2.getPassword());
-		System.out.println(user2.getId() + " 조회 성공");
-		
+		if(!user.getName().equals(user2.getName())) {
+			System.out.println("테스트 실패 (name)");
+		} else if(!user.getPassword().equals(user2.getPassword())) {
+			System.out.println("테스트 실패 (password)");
+		} else {
+			System.out.println("조회 테스트 성공");
+		}		
 	}
 	
 }
