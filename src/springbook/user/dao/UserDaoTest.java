7f9a6910eda8e831fb11fe12339161c5f3124301
Rupinel <rupinel@gmail.com>
2017-07-21 18:02:03 +0900

    170721 expected를 통한 예외던지기

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -22,21 +22,23 @@ public class UserDaoTest {
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao",UserDao.class);
 		
+		User user1 = new User("aaaa","shin","spring1");
+		User user2 = new User("bbbb","jo","spring2");
+		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 	
-		User user = new User();
-		user.setId("gyumee");
-		user.setName("test");
-		user.setPassword("springtest");
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
 		
-		dao.add(user);
-		assertThat(dao.getCount(), is(1));
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
 		
-		User user2 = dao.get(user.getId());
-	
-		assertThat(user2.getName(), is(user2.getName()));
-		assertThat(user2.getPassword(), is(user2.getPassword()));
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
 		
 	}
 	
@@ -66,4 +68,15 @@ public class UserDaoTest {
 		assertThat(dao.getCount(), is(3));
 	}
 	
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getUserFailure() throws SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao",UserDao.class);
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.get("unknown_id");
+		
+	}
 }
